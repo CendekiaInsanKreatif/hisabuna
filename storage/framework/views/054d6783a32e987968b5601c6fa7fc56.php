c912057435933de1fc4812e50c32162b05bb1414
Hisabuna_Dede <hisabuna@vps-92be05c4.vps.ovh.ca>
2024-07-15 20:48:47 +0000

    update

---
+++ storage/framework/views/054d6783a32e987968b5601c6fa7fc56.php
@@ -0,0 +1,105 @@
+<?php if (isset($component)) { $__componentOriginal9ac128a9029c0e4701924bd2d73d7f54 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54 = $attributes; } ?>
+<?php $component = App\View\Components\AppLayout::resolve([] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('app-layout'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\App\View\Components\AppLayout::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+    <?php $__env->startSection('content'); ?>
+    <form method="GET" action="<?php echo e(route('report.bukubesar')); ?>" class="mb-6">
+        <div class="flex space-x-2 items-end mb-4">
+            <div class="flex flex-col">
+                <label for="tanggal_mulai" class="block text-sm font-medium text-gray-700">Tanggal Mulai:</label>
+                <input type="date" id="tanggal_mulai" name="tanggal_mulai" value="<?php echo e($tanggalMulai); ?>" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
+            </div>
+            <div class="flex flex-col">
+                <label for="tanggal_selesai" class="block text-sm font-medium text-gray-700">Tanggal Selesai:</label>
+                <input type="date" id="tanggal_selesai" name="tanggal_selesai" value="<?php echo e($tanggalSelesai); ?>" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
+            </div>
+            <div class="flex flex-col">
+                <label for="akun" class="block text-sm font-medium text-gray-700">Akun:</label>
+                <input type="text" id="akun" name="akun" value="<?php echo e($akun); ?>" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
+            </div>
+            <div class="flex items-end">
+                <?php if (isset($component)) { $__componentOriginald411d1792bd6cc877d687758b753742c = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginald411d1792bd6cc877d687758b753742c = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.primary-button','data' => ['class' => 'h-10']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('primary-button'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['class' => 'h-10']); ?>Tampilkan <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginald411d1792bd6cc877d687758b753742c)): ?>
+<?php $attributes = $__attributesOriginald411d1792bd6cc877d687758b753742c; ?>
+<?php unset($__attributesOriginald411d1792bd6cc877d687758b753742c); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginald411d1792bd6cc877d687758b753742c)): ?>
+<?php $component = $__componentOriginald411d1792bd6cc877d687758b753742c; ?>
+<?php unset($__componentOriginald411d1792bd6cc877d687758b753742c); ?>
+<?php endif; ?>
+                <a href="<?php echo e(route('report.bukubesar.download')); ?>" class="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Download</a>
+            </div>
+        </div>
+    </form>
+
+    <div id="ledgerContent" class="w-full">
+        <div class="text-center">
+            <h1 class="text-2xl font-bold text-gray-800 mb-4">BUKU BESAR (LEDGER)</h1>
+            <h3 class="text-gray-700 mb-4">Periode <?php echo e($tanggalMulai); ?> s/d <?php echo e($tanggalSelesai); ?></h3>
+        </div>
+        <?php $__currentLoopData = $ledgers; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $coaAkun => $transactions): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <h2 class="text-xl font-semibold text-gray-700"><?php echo e($coaAkun); ?></h2>
+            <table class="w-full divide-y divide-gray-200 mt-4 mb-6 table-fixed">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col" class="w-1/5 px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Tanggal</th>
+                        <th scope="col" class="w-2/5 px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Keterangan</th>
+                        <th scope="col" class="w-1/6 px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Debit<div class="text-red-500">Bertambah</div></th>
+                        <th scope="col" class="w-1/6 px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Kredit<div class="text-red-500">Berkurang</div></th>
+                        <th scope="col" class="w-1/6 px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Saldo</th>
+                    </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                    <?php $__currentLoopData = $transactions; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $transaction): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <tr>
+                            <td class="px-4 py-2 whitespace-nowrap text-sm text-gray-500"><?php echo e(\Carbon\Carbon::parse($transaction->jurnal_tgl)->format('F d')); ?></td>
+                            <td class="px-4 py-2 whitespace-nowrap text-sm text-gray-500">
+                                <?php
+                                    $keterangan = $transaction->keterangan;
+                                    $max_length = 50;
+                                    $output = '';
+                                    while (strlen($keterangan) > $max_length) {
+                                        $output .= substr($keterangan, 0, $max_length) . '<br>';
+                                        $keterangan = substr($keterangan, $max_length);
+                                    }
+                                    $output .= $keterangan;
+                                    echo $output;
+                                ?>
+                            </td>
+                            <td class="px-4 py-2 whitespace-nowrap text-sm text-gray-500"><?php echo e(number_format($transaction->debit, 0, ',', '.')); ?></td>
+                            <td class="px-4 py-2 whitespace-nowrap text-sm text-gray-500"><?php echo e(number_format($transaction->credit, 0, ',', '.')); ?></td>
+                            <td class="px-4 py-2 whitespace-nowrap text-sm text-gray-500"><?php echo e(number_format($transaction->saldo, 0, ',', '.')); ?></td>
+                        </tr>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </tbody>
+            </table>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </div>
+    <?php $__env->stopSection(); ?>
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54)): ?>
+<?php $attributes = $__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54; ?>
+<?php unset($__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal9ac128a9029c0e4701924bd2d73d7f54)): ?>
+<?php $component = $__componentOriginal9ac128a9029c0e4701924bd2d73d7f54; ?>
+<?php unset($__componentOriginal9ac128a9029c0e4701924bd2d73d7f54); ?>
+<?php endif; ?>
+<?php /**PATH /var/www/hisabuna/resources/views/report/bukubesar.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/9f805f23d7a7935d7e734c2b6d55902e.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Jurnal</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+            color: #333;
+        }
+        .container {
+            width: 100%;
+            margin: auto;
+            background-color: #fff;
+            padding: 20px;
+            box-shadow: 0 0 10px rgba(0,0,0,0.1);
+        }
+
+        .logo {
+            width: 50px;
+            height: 50px;
+            background-color: #000;
+            margin: 0;
+            padding: 0;
+            margin-left: 30px;
+            align-self: flex-start;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 8px;
+            font-size: 12px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        th:nth-child(1), th:nth-child(4), th:nth-child(8) {
+            text-align: center;
+        }
+        td:nth-child(1), td:nth-child(8) {
+            text-align: right;
+        }
+        .period {
+            text-align: center;
+            margin-top: 5px;
+            font-style: italic;
+        }
+    </style>
+</head>
+<body>
+    <div class="container" style="overflow-x: auto; overflow-y: hidden; display: flex; justify-content: center;">
+        <div class="header-content" style="text-align: center; width: 100%;">
+            <div id="titleHeader" class="header-text" style="display: flex; align-items: center; justify-content: center; flex-direction: row;">
+                <img src="https://cdn.pixabay.com/photo/2017/07/25/11/59/logo-2537871_1280.png" alt="Logo" class="logo" style="margin-right: 10px; align-self: center;">
+                <div>
+                    <h2 style="padding: 0; margin: 0; margin-top: 20px;">PUBLIC VIRTUE</h2>
+                    <h4 style="padding: 0; margin: 0;">Institute for Digital Democracy and Civic Activism</h4>
+                </div>
+            </div>
+            <br>
+            <div class="title">
+                <h2>DAFTAR JURNAL</h2>
+                <p class="period">Periode 01/11/2023 s/d 30/11/2023</p>
+            </div>
+        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th rowspan="2">No.</th>
+                    <th colspan="2" style="text-align: center">Transaksi</th>
+                    <th rowspan="2" style="text-align: center">Keterangan Jurnal</th>
+                    <th colspan="3">Nomor Jurnal</th>
+                    <th rowspan="2">Jumlah</th>
+                </tr>
+                <tr>
+                    <th>Nomor</th>
+                    <th>Tanggal</th>
+                    <th>RV</th>
+                    <th>PV</th>
+                    <th>JV</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $__currentLoopData = $jurnal; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <tr style="<?php echo e($item->jenis == 'RV' ? 'background-color: #fee2e2;' : ($item->jenis == 'PV' ? 'background-color: #f4f4f5;' : ($item->jenis == 'JV' ? 'background-color: #fef9c3;' : ''))); ?>">
+                        <td><?php echo e($loop->iteration); ?></td>
+                        <td style="text-align: center"><?php echo e($loop->iteration); ?></td>
+                        <td><?php echo e(\Carbon\Carbon::parse($item->jurnal_tgl)->format('j/m/y')); ?></td>
+                        <td><?php echo e($item->keterangan); ?></td>
+                        <td><?php echo e($item->jenis == 'RV' ? $item->no_transaksi : ''); ?></td>
+                        <td><?php echo e($item->jenis == 'PV' ? $item->no_transaksi : ''); ?></td>
+                        <td><?php echo e($item->jenis == 'JV' ? $item->no_transaksi : ''); ?></td>
+                        <td><?php echo e(number_format($item->subtotal, 0, ',', '.')); ?></td>
+                    </tr>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
+<?php /**PATH /var/www/hisabuna/resources/views/report/daftarjurnal.blade.php ENDPATH**/ ?>

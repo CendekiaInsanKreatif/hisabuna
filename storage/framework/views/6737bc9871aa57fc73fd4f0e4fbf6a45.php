615251ae832fcc52ebcb4ab1b1862524c40f35fb
CendekiaInsanKreatif <cendekiainsankreatif@gmail.com>
2024-09-25 17:03:43 +0000

    up

---
+++ storage/framework/views/6737bc9871aa57fc73fd4f0e4fbf6a45.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+
+        <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+        <link rel="icon" href="<?php echo e(asset('images/icons/hisabuna-favicon.png')); ?>" type="image/x-icon">
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        
+        <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.3/themes/base/jquery-ui.css">
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+            integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/ui/1.13.3/jquery-ui.js"></script>
+
+        <!-- Scripts -->
+        <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
+        <?php echo $__env->yieldPushContent('script'); ?>
+    </head>
+    <body class="">
+        <?php echo e($slot); ?>
+
+    </body>
+</html>
+<?php /**PATH /var/www/trial_hisabuna/backend/resources/views/layouts/guest.blade.php ENDPATH**/ ?>

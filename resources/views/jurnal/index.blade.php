841a5b4a5e2c0483d7777e389d79b4d7e3365233
Dede Febriansyah <febriansyahd65@gmail.com>
2025-10-20 07:58:19 +0000

    update delete note, jurnal daftar

--- resources/views/jurnal/index.blade.php
+++ resources/views/jurnal/index.blade.php
@@ -416,10 +416,10 @@
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                     d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                             </svg>
-                                            <div class="text-sm text-info-700">
+                                            {{-- <div class="text-sm text-info-700">
                                                 <p class="font-medium">Catatan:</p>
                                                 <p>Modal akan tertutup otomatis setelah download selesai.</p>
-                                            </div>
+                                            </div> --}}
                                         </div>
                                     </div>
                                 </div>
